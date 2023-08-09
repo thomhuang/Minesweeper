398556652e7a5e79a19ef20334a6e41e671e5574
thuang <thuang@costar.com>
2023-08-09 15:46:38 -0700

    full (most) feature additions -organized project structure -cleaned variables and code -added ability to reset game -improved fill/reveal algorithm to reveal an extra layer -notifies user when won -type checking on user prompts -helper functions

--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -1,85 +1,129 @@
-﻿using System.Drawing;
-using System.Text.RegularExpressions;
-using static Minesweeper.Utility;
-namespace Minesweeper
+﻿namespace Minesweeper
 {
-    public class Game
+    internal class Game
     {
-        private readonly int _width;
-        private readonly int _height;
-        private readonly int _bombs;
-        private readonly int _numMoves = 0;
-        public Cell[,] game;
+        private int rowMax;
+        private int colMax;
+        private int numMoves = 0;
         public Board board;
-        public Game(int width, int height, int bombs)
+
+        public Game()
         {
-            _width = width;
-            _height = height;
-            _bombs = bombs;
+            newGame();
+        }
 
-            board = new Board(_width, _height, _bombs);
+        public void newGame()
+        {
+            int userRows, userColumns, userMines;
+            Console.Write("How many rows would you like? ");
+            while (!int.TryParse(Console.ReadLine(), out userRows) || userRows >= 100)
+            {
+                string err = userRows >= 100
+                    ? "You've requested too many rows."
+                    : "That was an invalid row number. Please enter a valid value: ";
+                Console.WriteLine(err);
+            }
+            rowMax = userRows;
 
+            Console.Write("How many columns would you like? ");
+            while (!int.TryParse(Console.ReadLine(), out userColumns) || userColumns >= 100)
+            {
+                string err = userColumns >= 100
+                    ? "You've requested too many columns."
+                    : "That was an invalid column number. Please enter a valid value: ";
+                Console.WriteLine(err);
+            }
+            colMax = userColumns;
+
+            Console.Write("How many mines would you like total? ");
+            while (!int.TryParse(Console.ReadLine(), out userMines) || (userMines >= (userRows * userColumns)))
+            {
+                Console.WriteLine("That was an invalid mine number. Please enter a valid value: ");
+            }
+
+            board = new Board(userRows, userColumns, userMines);
+            board.printBoard();
         }
 
         public void setFlag(int r, int c)
         {
-            board.board[r, c].display = FlagChar;
+            board.setType(r, c, Constants.FLAG);
+        }
+
+        public void setMine(int r, int c)
+        {
+            board.setType(r, c, Constants.MINE);
         }
 
         public void play()
         {
-            Console.WriteLine("What would you like to do? (s)elect, (f)lag, (p)rint, (c)heat, or (q)uit");
             while (true)
             {
+                Console.WriteLine("What would you like to do? (s)elect, (f)lag, (p)rint, (c)heat, (r)eset, or (q)uit");
                 char userAction = Console.ReadKey().KeyChar;
                 Console.WriteLine();
                 switch (userAction)
                 {
                     case 's':
                         Console.WriteLine("Please enter your desired row: ");
-
                         int selectedRow;
-                        while(!int.TryParse(Console.ReadLine(), out selectedRow) || (selectedRow < 0 || selectedRow > _width))
+                        while (!int.TryParse(Console.ReadLine(), out selectedRow) || (selectedRow <= 0 || selectedRow > rowMax))
                         {
-                            Console.WriteLine("That was an invalid row number. Please enter a valid value.");
+                            Console.WriteLine("That was an invalid row number. Please enter a valid value: ");
                         }
 
                         Console.WriteLine("Please enter your desired column: ");
                         int selectedColumn;
-                        while (!int.TryParse(Console.ReadLine(), out selectedColumn) || (selectedColumn < 0 || selectedColumn > _height))
+                        while (!int.TryParse(Console.ReadLine(), out selectedColumn) || (selectedColumn <= 0 || selectedColumn > colMax))
                         {
-                            Console.WriteLine("That was an invalid column number. Please enter a valid value.");
+                            Console.WriteLine("That was an invalid column number. Please enter a valid value: ");
                         }
 
-                        if (board.isBomb(selectedRow - 1, selectedColumn - 1) == 1)
+                        if (board.isMine(selectedRow - 1, selectedColumn - 1))
                         {
-                            board.board[selectedRow - 1, selectedColumn - 1].type = Bomb;
                             Console.WriteLine("Game over!");
                             Environment.Exit(0);
                         }
-                        else {
-
-                            board.revealCell(selectedRow - 1, selectedColumn - 1);
+                        else if (numMoves == 0)
+                        {
+                            board.populateBoard(selectedRow - 1, selectedColumn - 1);
                         }
+                        board.revealCell(selectedRow - 1, selectedColumn - 1);
 
+                        board.printBoard();
+                        numMoves += 1;
+                        if (board.gameWon())
+                        {
+                            Console.WriteLine($"You've won in {numMoves} selections, good job!");
+                            board.printRevealedBoard();
+                            Environment.Exit(0);
+                        }
                         break;
                     case 'f':
                         Console.WriteLine("Please enter your desired row: ");
                         int flaggedRow;
-                        while (!int.TryParse(Console.ReadLine(), out flaggedRow) || (flaggedRow < 0 || flaggedRow > _width))
+                        while (!int.TryParse(Console.ReadLine(), out flaggedRow) || (flaggedRow <= 0 || flaggedRow > rowMax))
                         {
-                            Console.WriteLine("That was an invalid row number. Please enter a valid value.");
+                            Console.WriteLine("That was an invalid row number. Please enter a valid value: ");
                         }
                         Console.WriteLine("Please enter your desired column: ");
                         int flaggedColumn;
-                        while (!int.TryParse(Console.ReadLine(), out flaggedColumn) || (flaggedColumn < 0 || flaggedColumn > _height))
+                        while (!int.TryParse(Console.ReadLine(), out flaggedColumn) || (flaggedColumn <= 0 || flaggedColumn > colMax))
                         {
-                            Console.WriteLine("That was an invalid column number. Please enter a valid value.");
+                            Console.WriteLine("That was an invalid column number. Please enter a valid value: ");
                         }
                         setFlag(flaggedRow - 1, flaggedColumn - 1);
+                        board.printBoard();
                         break;
                     case 'c':
-                        board.printRevealedBoard();
+                        if (numMoves == 0)
+                        {
+                            Console.WriteLine("Please enter a move first before revealing the board!");
+                        }
+                        else
+                        {
+                            board.printRevealedBoard();
+                        }
                         break;
                     case 'p':
                         board.printBoard();
@@ -87,28 +131,17 @@ namespace Minesweeper
                     case 'q':
                         Environment.Exit(0);
                         break;
+                    case 'r':
+                        Console.WriteLine("The board has reset! Please enter the new properties you'd like for the next board: ");
+                        newGame();
+                        break;
                     default:
-                        Console.WriteLine("Please enter a valid action, would you like to (s)elect, (f)lag, (p)rint, (c)heat, or (q)uit");
-                        Console.WriteLine("");
+                        Console.WriteLine("Please enter a valid action!");
+                        Console.WriteLine();
                         break;
                 }
-                board.printBoard();
 
             }
-        }
-
-        public static void Main(string[] args)
-        {
-            Console.Write("How many rows would you like? ");
-            var userRows = Convert.ToInt32(Console.ReadLine());
-            Console.Write("How many columns would you like? ");
-            var userColumns = Convert.ToInt32(Console.ReadLine());
-            Console.Write("How many mines would you like total? ");
-            var userMines = Convert.ToInt32(Console.ReadLine());
-            Game currGame = new Game(userRows, userColumns, userMines);
-            currGame.board.populateMines();
-            currGame.board.printBoard();
-            currGame.play();
         }
     }
 }
