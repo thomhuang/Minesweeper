b49be182e5b304007f0096ab894d8efeb1c49bbf
Thomas Huang <81591042+thomhuang@users.noreply.github.com>
2023-08-15 16:33:32 -0700

    improvements to code quality and game qol (#2)
    Co-authored-by: thuang <thuang@costar.com>

--- Minesweeper/Board.cs
+++ Minesweeper/Board.cs
@@ -1,23 +1,30 @@
 ﻿using System.Drawing;
+using System.Text;
+
 namespace Minesweeper
 {
-    public struct Cell
-    {
-        public bool visited { get; set; }
-        public int type { get; set; }
-        public string display { get; set; }
-        public int mineCount { get; set; }
-        public override string ToString()
-        {
-            return display;
-        }
-    }
     internal class Board
     {
+        private struct Cell
+        {
+            public bool visited { get; set; }
+            public int type { get; set; }
+            public string display { get; set; }
+            public int mineCount { get; set; }
+            public override string ToString()
+            {
+                return display;
+            }
+        }
+
+        // Used to grab surrounding 8 neighbors
+        private static readonly int[] deltaX = { 0, 0, 1, 1, -1, -1, 1, -1 };
+        private static readonly int[] deltaY = { 1, -1, 0, -1, 0, -1, 1, 1 };
+
         private readonly int _rowMax;
         private readonly int _colMax;
         private readonly int _mines;
-        private Cell[,] board;
+        private Cell[,] _board;
 
         public Board(int rows, int columns, int mines)
         {
@@ -25,15 +32,15 @@ namespace Minesweeper
             _colMax = columns;
             _mines = mines;
 
-            board = new Cell[rows, columns];
+            _board = new Cell[rows, columns];
             // set default cell properties
             for (int r = 0; r < rows; r++)
             {
                 for (int c = 0; c < columns; c++)
                 {
-                    board[r, c].visited = false;
+                    _board[r, c].visited = false;
                     setType(r, c, Constants.HIDDEN);
-                    board[r, c].mineCount = 0;
+                    _board[r, c].mineCount = 0;
                 }
             }
         }
@@ -79,9 +86,6 @@ namespace Minesweeper
         private void countNeighboringMines(int r, int c)
         {
             // Loop through all surrounding cells, if there's a mine surrounding increase mine count
-            int[] deltaX = new int[] { 0, 0, 1, 1, -1, -1, 1, -1 };
-            int[] deltaY = new int[] { 1, -1, 0, -1, 0, -1, 1, 1 };
-
             for (int i = 0; i < 8; i++)
             {
                 int currX = r + deltaX[i];
@@ -89,20 +93,17 @@ namespace Minesweeper
 
                 if (currX >= 0 && currX < _rowMax && currY >= 0 && currY < _colMax)
                 {
-                    board[r, c].mineCount += isMine(currX, currY) ? 1 : 0;
+                    _board[r, c].mineCount += isMine(currX, currY) ? 1 : 0;
                 }
             }
         }
 
-        public bool isMine(int r, int c)
-        {
-            return board[r, c].type == Constants.MINE;
-        }
+        public bool isMine(int r, int c) => _board[r, c].type == Constants.MINE;
 
         // BFS flood fill for selected cells
         public void revealCell(int r, int c)
         {
-            if (board[r, c].type == Constants.SHOWN)
+            if (_board[r, c].type == Constants.SHOWN)
             {
                 Console.WriteLine("You've already selected this cell! Please choose another one or do another action.");
                 return;
@@ -119,14 +120,11 @@ namespace Minesweeper
                 Point currPoint = queue.Dequeue();
                 setType(currPoint.X, currPoint.Y, Constants.SHOWN);
                 // Allows us to fill until first cells that have some neighboring mine
-                if (board[currPoint.X, currPoint.Y].mineCount != 0)
+                if (_board[currPoint.X, currPoint.Y].mineCount != 0)
                 {
                     continue;
                 }
                 // Loop through all 8 adjacent points
-                int[] deltaX = new int[] { 0, 0, 1, 1, -1, -1, 1, -1 };
-                int[] deltaY = new int[] { 1, -1, 0, -1, 0, -1, 1, 1 };
-
                 for (int i = 0; i < 8; i++)
                 {
                     int neighborX = currPoint.X + deltaX[i];
@@ -135,7 +133,7 @@ namespace Minesweeper
                     if (neighborX >= 0 && neighborX < _rowMax && neighborY >= 0 && neighborY < _colMax)
                     {
                         Point pointToAdd = new Point(neighborX, neighborY);
-                        if (board[neighborX, neighborY].type == Constants.HIDDEN)
+                        if (_board[neighborX, neighborY].type == Constants.HIDDEN)
                         {
                             queue.Enqueue(pointToAdd);
                         }
@@ -146,10 +144,10 @@ namespace Minesweeper
 
         public bool gameWon()
         {
-            // if hidden count is 0 while game is going on, game should be over with 'x' remaining mines on the board.
+            // if hidden count is 0 while game is going on, game should be over with 'x' remaining mines on the _board.
             int hiddenCount = 0;
 
-            foreach (var cell in board)
+            foreach (var cell in _board)
             {
                 if (cell.type == Constants.HIDDEN) { hiddenCount++; }
             }
@@ -160,95 +158,102 @@ namespace Minesweeper
             switch (type)
             {
                 case Constants.HIDDEN:
-                    board[r, c].type = type;
-                    board[r, c].display = "-";
+                    _board[r, c].type = type;
+                    _board[r, c].display = "-";
                     break;
                 case Constants.SHOWN:
-                    board[r, c].type = Constants.SHOWN;
-                    board[r, c].display = board[r, c].mineCount.ToString();
+                    _board[r, c].type = Constants.SHOWN;
+                    _board[r, c].display = _board[r, c].mineCount.ToString();
                     break;
                 case Constants.FLAG:
-                    board[r, c].display = "F";
+                    _board[r, c].display = "F";
                     break;
                 case Constants.MINE:
-                    board[r, c].type = type;
-                    board[r, c].display = "*";
+                    _board[r, c].type = type;
+                    _board[r, c].display = "*";
                     break;
             }
         }
 
         public void printBoard()
         {
-            string res = "\n\t";
+            var sb = new StringBuilder("\n\t");
             // index the columns
             for (int i = 1; i < _colMax + 1; i++)
             {
-                res += $"{i} ";
+                sb.Append($"{i} ");
             }
-            res += "\n\n";
+            sb.Append("\n\n");
             for (int i = 0; i < _rowMax; i++)
             {
                 // index the rows
-                res += (i + 1) + "\t";
+                sb.Append((i + 1) + "\t");
                 for (int j = 0; j < _colMax; j++)
                 {
-                    // If cell is type shown, show its mine count
-                    if (board[i, j].type == Constants.SHOWN)
+                    switch (_board[i,j].type)
                     {
-                        res += $"{board[i, j].mineCount} ";
+                        case Constants.SHOWN:
+                            sb.Append($"{_board[i, j].mineCount} ");
+                            break;
+                        case Constants.MINE:
+                            break;
+                    }
+                    // If cell is type shown, show its mine count
+                    if (_board[i, j].type == Constants.SHOWN)
+                    {
                     }
                     // If it's a mine and user hasn't flagged, then show hidden character
-                    else if (board[i, j].type == Constants.MINE && board[i, j].display != Constants.FLAGCHAR)
+                    else if (_board[i, j].type == Constants.MINE && _board[i, j].display != Constants.FLAGCHAR)
                     {
-                        res += $"{Constants.HIDDENCHAR} ";
+                        sb.Append($"{Constants.HIDDENCHAR} ");
                     }
                     // Otherwise show its default display, e.g. flags, hidden, etc.
                     else
                     {
-                        res += $"{board[i, j]} ";
+                        sb.Append($"{_board[i, j]} ");
                     }
 
                     // If indices are 2 integers, increase spacing, we only allow for row/columns up to 99.
                     if (j >= 9)
                     {
-                        res += " ";
+                        sb.Append(" ");
 
                     }
                 }
-                res += "\n";
+                sb.Append("\n");
             }
-            Console.WriteLine(res);
+            Console.WriteLine(sb.ToString());
         }
 
         public void printRevealedBoard()
         {
-            string res = "\n\t";
+            var sb = new StringBuilder("\n\t");
             // index the columns
             for (int i = 1; i < _colMax + 1; i++)
             {
-                res += $"{i} ";
+                sb.Append($"{i} ");
             }
-            res += "\n\n";
+            sb.Append("\n\n");
             for (int i = 0; i < _rowMax; i++)
             {
                 // index the rows
-                res += (i + 1) + "\t";
+                sb.Append((i + 1) + "\t");
                 for (int j = 0; j < _colMax; j++)
                 {
                     // If not a mine, show mine count, otherwise show mine symbol.
-                    res += board[i, j].type != Constants.MINE
-                        ? $"{board[i, j].mineCount} "
-                        : $"{board[i, j]} ";
+                    sb.Append(_board[i, j].type != Constants.MINE
+                        ? $"{_board[i, j].mineCount} "
+                        : $"{_board[i, j]} ");
                     // If indices are 2 integers, increase spacing, we only allow for row/columns up to 99.
                     if (j > 9)
                     {
-                        res += " ";
+                        sb.Append(" ");
 
                     }
                 }
-                res += "\n";
+                sb.Append("\n");
             }
-            Console.WriteLine(res);
+            Console.WriteLine(sb.ToString());
         }
     }
 }
--- Minesweeper/Game.cs
+++ Minesweeper/Game.cs
@@ -7,66 +7,51 @@
         private int numMoves = 0;
         public Board board;
 
-        public Game()
-        {
-            newGame();
-        }
+        public Game() => newGame();
 
         private void newGame()
         {
             int userRows, userColumns, userMines;
-            Console.Write("How many rows would you like? ");
-            while (!int.TryParse(Console.ReadLine(), out userRows) || userRows >= 100)
+            Console.Write("How many rows would you like? The range of rows is [1,99]. ");
+            while (!int.TryParse(Console.ReadLine(), out userRows) || userRows < 1 || userRows >= 100)
             {
-                string err = userRows >= 100
-                    ? "You've requested too many rows."
-                    : "That was an invalid row number. Please enter a valid value: ";
-                Console.WriteLine(err);
+                Console.Write("That was an invalid integer. Please enter a valid value: ");
             }
+
             rowMax = userRows;
 
-            Console.Write("How many columns would you like? ");
-            while (!int.TryParse(Console.ReadLine(), out userColumns) || userColumns >= 100)
+            Console.Write("How many columns would you like? The range of columns is [1, 99]. ");
+            while (!int.TryParse(Console.ReadLine(), out userColumns) || userColumns < 1 || userColumns >= 100)
             {
-                string err = userColumns >= 100
-                    ? "You've requested too many columns."
-                    : "That was an invalid column number. Please enter a valid value: ";
-                Console.WriteLine(err);
+                Console.Write("That was an invalid integer. Please enter a valid value: ");
             }
             colMax = userColumns;
 
             Console.Write("How many mines would you like total? ");
             while (!int.TryParse(Console.ReadLine(), out userMines) || (userMines >= (userRows * userColumns)))
             {
-                Console.WriteLine("That was an invalid mine number. Please enter a valid value: ");
+                Console.Write("That was an invalid mine number. Please enter a valid value: ");
             }
 
             board = new Board(userRows, userColumns, userMines);
             board.printBoard();
         }
 
-        private void setFlag(int r, int c)
-        {
-            board.setType(r, c, Constants.FLAG);
-        }
-
-        private void setMine(int r, int c)
-        {
-            board.setType(r, c, Constants.MINE);
-        }
+        private void setFlag(int r, int c) => board.setType(r, c, Constants.FLAG);
 
         public void play()
         {
             while (true)
             {
                 Console.WriteLine("What would you like to do? (s)elect, (f)lag, (p)rint, (c)heat, (r)eset, or (q)uit");
-                char userAction = Console.ReadKey().KeyChar;
+                string userAction = Console.ReadKey().KeyChar.ToString();
                 Console.WriteLine();
-                switch (userAction)
+                switch (userAction.ToLower())
                 {
-                    case 's':
+                    case "s":
                         Console.WriteLine("Please enter your desired row: ");
                         int selectedRow;
+
                         while (!int.TryParse(Console.ReadLine(), out selectedRow) || (selectedRow <= 0 || selectedRow > rowMax))
                         {
                             Console.WriteLine("That was an invalid row number. Please enter a valid value: ");
@@ -74,6 +59,7 @@
 
                         Console.WriteLine("Please enter your desired column: ");
                         int selectedColumn;
+
                         while (!int.TryParse(Console.ReadLine(), out selectedColumn) || (selectedColumn <= 0 || selectedColumn > colMax))
                         {
                             Console.WriteLine("That was an invalid column number. Please enter a valid value: ");
@@ -89,25 +75,30 @@
                             board.populateBoard(selectedRow - 1, selectedColumn - 1);
                         }
                         board.revealCell(selectedRow - 1, selectedColumn - 1);
+                        numMoves += 1;
 
                         board.printBoard();
-                        numMoves += 1;
+
                         if (board.gameWon())
                         {
                             Console.WriteLine($"You've won in {numMoves} selections, good job!");
                             board.printRevealedBoard();
                             Environment.Exit(0);
                         }
+
                         break;
-                    case 'f':
+                    case "f":
                         Console.WriteLine("Please enter your desired row: ");
                         int flaggedRow;
+
                         while (!int.TryParse(Console.ReadLine(), out flaggedRow) || (flaggedRow <= 0 || flaggedRow > rowMax))
                         {
                             Console.WriteLine("That was an invalid row number. Please enter a valid value: ");
                         }
+
                         Console.WriteLine("Please enter your desired column: ");
                         int flaggedColumn;
+
                         while (!int.TryParse(Console.ReadLine(), out flaggedColumn) || (flaggedColumn <= 0 || flaggedColumn > colMax))
                         {
                             Console.WriteLine("That was an invalid column number. Please enter a valid value: ");
@@ -115,7 +106,7 @@
                         setFlag(flaggedRow - 1, flaggedColumn - 1);
                         board.printBoard();
                         break;
-                    case 'c':
+                    case "c":
                         if (numMoves == 0)
                         {
                             Console.WriteLine("Please enter a move first before revealing the board!");
@@ -125,19 +116,18 @@
                             board.printRevealedBoard();
                         }
                         break;
-                    case 'p':
+                    case "p":
                         board.printBoard();
                         break;
-                    case 'q':
+                    case "q":
                         Environment.Exit(0);
                         break;
-                    case 'r':
+                    case "r":
                         Console.WriteLine("The board has reset! Please enter the new properties you'd like for the next board: ");
                         newGame();
                         break;
                     default:
                         Console.WriteLine("Please enter a valid action!");
-                        Console.WriteLine();
                         break;
                 }
 
